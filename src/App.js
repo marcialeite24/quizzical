67bcc965024a897b9aafea30ab09191ba01e857d
marcialeite24 <marciasilvaleite@hotmail.com>
2024-05-08 19:29:20 +0100

    Created the start screen of the quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import StartGame from './components/StartGame'
 
 export default function App() {
   return (
-    <main>
-
+    <main className='start-game'>
+      <img src={require('./images/shape-top.png')} alt='shape-top' className='shape-top'/>
+      <StartGame />
+      <img src={require('./images/shape-bottom.png')} alt='shape-bottom' className='shape-bottom'/>
     </main>
   );
 }
---
+++ src/components/StartGame.js
@@ -0,0 +1,9 @@
+export default function StartGame() {
+    return (
+        <div>
+            <h1>Quizzical</h1>
+            <p>Answer all the questions and have fun! To start press the button.</p>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
