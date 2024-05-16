ce3f8590207d2466c94bd990d8b7df70e0c7fd56
marcialeite24 <marciasilvaleite@hotmail.com>
2024-05-16 21:50:48 +0100

    Fetching api questions according to selected caterogy & difficulty

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,30 +4,30 @@ export default function Category(props) {
             <label htmlFor="category" className="dropdown-category-label">Category: </label>
             <select id="dropdown-category" name="category" value={props.data} onChange={props.handleChange}>
                 <option value="">Select a category</option>
-                <option value="option-1">General Knowledge</option>
-                <option value="option-2">Entertainment: Books</option>
-                <option value="option-3">Entertainment: Film</option>
-                <option value="option-4">Entertainment: Music</option>
-                <option value="option-5">Entertainment: Musicals & Theatres</option>
-                <option value="option-6">Entertainment: Television</option>
-                <option value="option-7">Entertainment: Video Games</option>
-                <option value="option-8">Entertainment: Board Games</option>
-                <option value="option-9">Science & Nature</option>
-                <option value="option-10">Science: Computers</option>
-                <option value="option-11">Science: Mathematics</option>
-                <option value="option-12">Mythology</option>
-                <option value="option-13">Sports</option>
-                <option value="option-14">Geography</option>
-                <option value="option-15">History</option>
-                <option value="option-16">Politics</option>
-                <option value="option-17">Art</option>
-                <option value="option-18">Celebrities</option>
-                <option value="option-19">Animals</option>
-                <option value="option-20">Vehicles</option>
-                <option value="option-21">Entertainment: Comics</option>
-                <option value="option-22">Science: Gadgets</option>
-                <option value="option-23">Entertainment: Japanese Anime & Manga</option>
-                <option value="option-24">Entertainment: Cartoon & Animations</option>
+                <option value="1">General Knowledge</option>
+                <option value="2">Entertainment: Books</option>
+                <option value="3">Entertainment: Film</option>
+                <option value="4">Entertainment: Music</option>
+                <option value="5">Entertainment: Musicals & Theatres</option>
+                <option value="6">Entertainment: Television</option>
+                <option value="7">Entertainment: Video Games</option>
+                <option value="8">Entertainment: Board Games</option>
+                <option value="9">Science & Nature</option>
+                <option value="10">Science: Computers</option>
+                <option value="11">Science: Mathematics</option>
+                <option value="12">Mythology</option>
+                <option value="13">Sports</option>
+                <option value="14">Geography</option>
+                <option value="15">History</option>
+                <option value="16">Politics</option>
+                <option value="17">Art</option>
+                <option value="18">Celebrities</option>
+                <option value="19">Animals</option>
+                <option value="20">Vehicles</option>
+                <option value="21">Entertainment: Comics</option>
+                <option value="22">Science: Gadgets</option>
+                <option value="23">Entertainment: Japanese Anime & Manga</option>
+                <option value="24">Entertainment: Cartoon & Animations</option>
             </select>
         </div>        
     )
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,19 +1,35 @@
 import React from 'react'
 
 export default function Questions(props) {
+    console.log(props)
     const [questions, setQuestions] = React.useState([])
-
-         
+             
     React.useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple')
+        fetch(`https://opentdb.com/api.php?amount=5&category=${props.category}&difficulty=${props.difficulty}&type=multiple`)
         .then((res) => {
             return res.json();
         })
         .then((data) => {
             console.log(data);
-            setQuestions(data.results);
+            const encodedQuestions = data.results.map((question) => ({
+              ...question,
+              question: decodeEntities(question.question),
+              correct_answer: decodeEntities(question.correct_answer),
+              incorrect_answers: question.incorrect_answers.map(decodeEntities),
+            }));
+            console.log(encodedQuestions)
+            setQuestions(encodedQuestions)
         })
-    }, [])
+        .catch((error) => {
+            console.error('Error fetching data:', error)
+        })
+    }, [props.category, props.difficulty])
+
+    const decodeEntities = (encodedString) => {
+        const textArea = document.createElement('textarea');
+        textArea.innerHTML = encodedString;
+        return textArea.value;
+    }
 
     return (
         <div>      
